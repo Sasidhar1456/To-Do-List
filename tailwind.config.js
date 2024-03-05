d9110f4b9477796979326fd1a5eddd3cd5cb2eb4
Sasidhar Pinjala <sasidharpinjala@gmail.com>
2024-03-05 21:14:22 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary' : '#EDC7B7',
+        'secondary': '#123C69',
+        'third': '#EEE2DC',
+        'forth':' #AC3B61',
+      },
+      
+      fontFamily: {
+        'lucky' : ['Luckiest\\ Guy','sans-serif'],
+        'san' : 'sans-serif',
+        'love' : 'Love Ya Like A Sister',
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
