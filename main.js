6c1ad8235b28e6fadcb7d0aa2bf1f6dd32cc8ab8
Sasidhar Pinjala <sasidharpinjala@gmail.com>
2024-03-08 13:16:47 +0530

    sasi

--- main.js
+++ main.js
@@ -23,5 +23,76 @@ document.addEventListener("DOMContentLoaded",function(){
             
         }
 
-    },150)
+    },100)
 });
+
+function toggleTaskCompletion(checkbox) {
+    const taskText = checkbox.parentElement.nextElementSibling.firstElementChild;
+    taskText.classList.toggle('line-through', checkbox.checked);
+}
+
+function deleteTask(button){
+    const task = button.parentElement.parentElement;
+    if(confirm("You really want to delete it")){
+        task.remove();
+    } 
+}
+
+function addTask() {
+    // Get the input value
+    const taskInput = document.getElementById('task');
+    const taskText = taskInput.value;
+
+    // Create task elements
+    const taskContainer = document.getElementById('taskContainer');
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('flex', 'justify-center', 'items-center', 'mt-5');
+
+    
+    const checkDiv = document.createElement('div');
+    checkDiv.classList.add("mt-1");
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('h-5', 'w-5', 'checked:bg-forth', 'checked:border-transparent', 'focus:outline-none');
+    checkbox.onclick = function() {
+        toggleTaskCompletion(checkbox);
+    };
+
+    checkDiv.appendChild(checkbox);
+
+
+    const spanDiv = document.createElement("div");
+    spanDiv.classList.add("ml-3", "w-40");
+
+    const taskSpan = document.createElement('div');
+    taskSpan.classList.add('ml-3', 'w-40');
+    const taskTextSpan = document.createElement('span');
+    taskTextSpan.classList.add('text-secondary', 'text-xl', 'font-love', 'font-semibold', 'max-w-[200px]');
+    taskTextSpan.innerText = taskText;
+    taskSpan.appendChild(taskTextSpan);
+
+    spanDiv.appendChild(taskSpan);
+
+    
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("ml-3")
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('bg-forth', 'rounded-md', 'px-1');
+    deleteButton.onclick = function() {
+        deleteTask(deleteButton);
+    };
+    deleteButton.innerHTML = '<span class="text-third font-love">Delete</span>';
+
+    buttonDiv.appendChild(deleteButton);
+
+    // Append elements to the task container
+    taskDiv.appendChild(checkDiv);
+    taskDiv.appendChild(spanDiv);
+    taskDiv.appendChild(buttonDiv);
+    taskContainer.appendChild(taskDiv);
+
+    // Clear the input field
+    taskInput.value = '';
+}
